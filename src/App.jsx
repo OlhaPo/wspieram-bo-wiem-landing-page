695613b997177fbb1929520a6c37695856959c18
Olya <ponomaryova.olya.y@gmail.com>
2023-10-25 10:43:01 +0200

    Create new components for reviews and contacts

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import MainPageSlideShow from "./components/MainPageSlideShow";
 import NavMenu from "./components/NavMenu";
+import Reviews from "./components/Reviews";
 
 function App() {
   return (
     <div>
       <NavMenu />
       <MainPageSlideShow />
+      <Reviews />
     </div>
   );
 }
---
+++ src/components/Reviews.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Reviews = () => {
+  return <div className="reviews">TODO: Тут будуть відгуки</div>;
+};
+
+export default Reviews;
