d3109e8add052fa2cef80c257afc1feb543f393e
Olya <ponomaryova.olya.y@gmail.com>
2023-10-26 13:13:19 +0200

    Add styling to slides description

--- src/components/MainPageSlideShow.jsx
+++ src/components/MainPageSlideShow.jsx
@@ -6,56 +6,63 @@ import { AiOutlineLeft, AiOutlineRight } from "react-icons/ai";
 const MainPageSlideShow = () => {
   return (
     <div>
-      <Carousel
-        showStatus={false}
-        dynamicHeight={true}
-        renderArrowPrev={(clickHandler, hasPrev) => {
-          return (
-            <div
-              className={`${
-                hasPrev ? "absolute" : "hidden"
-              } top-0 bottom-0 left-0 flex justify-center items-center p-3 opacity-30 hover:opacity-100 cursor-pointer z-20`}
-              onClick={clickHandler}
-            >
-              <AiOutlineLeft className="w-9 h-9 text-white" />
+      <div className="mx-auto max-w-[70rem]">
+        <Carousel
+          showStatus={false}
+          renderArrowPrev={(clickHandler, hasPrev) => {
+            return (
+              <div
+                className={`${
+                  hasPrev ? "absolute" : "hidden"
+                } top-0 bottom-0 left-0 flex justify-center items-center p-3 opacity-50 hover:opacity-100 cursor-pointer z-20`}
+                onClick={clickHandler}
+              >
+                <AiOutlineLeft className="w-6 h-6 lg:w-12 lg:h-12 text-white" />
+              </div>
+            );
+          }}
+          renderArrowNext={(clickHandler, hasNext) => {
+            return (
+              <div
+                className={`${
+                  hasNext ? "absolute" : "hidden"
+                } top-0 bottom-0 right-0 flex justify-center items-center p-3 opacity-50 hover:opacity-100 cursor-pointer z-20`}
+                onClick={clickHandler}
+              >
+                <AiOutlineRight className="w-6 h-6 lg:w-12 lg:h-12 text-white" />
+              </div>
+            );
+          }}
+        >
+          <div className="custom-slide">
+            <img src="./images/slide-1.png" alt="Slide 1" />
+            <div className="text-overlay left-[-20px] md:left-0 text-left text-base md:text-[130%] font-light top-0">
+              <p>Description for Slide 1</p>
+              <div className="mt-5">
+                <a href="">Дізнатися більше</a>
+              </div>
             </div>
-          );
-        }}
-        renderArrowNext={(clickHandler, hasNext) => {
-          return (
-            <div
-              className={`${
-                hasNext ? "absolute" : "hidden"
-              } top-0 bottom-0 right-0 flex justify-center items-center p-3 opacity-30 hover:opacity-100 cursor-pointer z-20`}
-              onClick={clickHandler}
-            >
-              <AiOutlineRight className="w-9 h-9 text-white" />
+          </div>
+          <div className="custom-slide">
+            <img src="./images/slide-2.png" alt="Slide 2" />
+            <div className="text-overlay left-[54%] text-left md:text-[130%] font-light top-0">
+              <p>Description for Slide 2</p>
+              <div className="mt-5">
+                <a href="">Дізнатися більше</a>
+              </div>
             </div>
-          );
-        }}
-      >
-        <div className="custom-slide">
-          <img src="./images/doula-background.png" alt="Slide 1" />
-          <div className="text-overlay">
-            <h2>Slide 1</h2>
-            <p>Description for Slide 1</p>
           </div>
-        </div>
-        <div className="custom-slide">
-          <img src="./images/postpartum-doula-background.png" alt="Slide 2" />
-          <div className="text-overlay">
-            <h2>Slide 2</h2>
-            <p>Description for Slide 2</p>
+          <div className="custom-slide">
+            <img src="./images/slide-3.png" alt="Slide 3" />
+            <div className="text-overlay left-[-20px] md:left-0 text-left text-base md:text-[130%] font-light top-0">
+              <p>Description for Slide 3</p>
+              <div className="mt-5">
+                <a href="">Дізнатися більше</a>
+              </div>
+            </div>
           </div>
-        </div>
-        <div className="custom-slide">
-          <img src="./images/family-therapy-background.png" alt="Slide 3" />
-          <div className="text-overlay">
-            <h2>Slide 3</h2>
-            <p>Description for Slide 3</p>
-          </div>
-        </div>
-      </Carousel>
+        </Carousel>
+      </div>
     </div>
   );
 };
--- src/components/NavMenu.jsx
+++ src/components/NavMenu.jsx
@@ -14,35 +14,46 @@ const NavMenu = () => {
       {isOpenNav ? (
         <div
           onClick={handleNav}
-          className="w-full bg-[#6a6867] min-h-screen z-[120] flex flex-col justify-center items-center gap-6"
+          className="w-full bg-[#eceae8] min-h-screen z-[120] flex flex-col justify-center items-center gap-6"
         >
           <AiOutlineClose
             size={28}
-            className="relative -right-[180px] -top-[220px]"
+            className="relative -right-[150px] -top-[250px]"
           />
           <a href="/about">Про мене</a>
           <a href="/services">Послуги</a>
           <a href="#contacts">Контакти</a>
+          <a href="#" className="link-button">
+            Запис на консультацію
+          </a>
         </div>
       ) : null}
-      <div className="flex flex-row justify-around items-center py-4">
-        <img
-          src="./logo.png"
-          alt="Wspieram bo wiem logo"
-          className="h-16 w-16"
-        />
-
-        <span className="text-xl md:text-3xl italic text-primary">
-          Wspieram, bo wiem...
-        </span>
+      <div className="max-w-[70rem] mx-auto flex flex-row justify-around xl:justify-between items-center py-4">
+        <div className="inline-flex gap-3 lg:gap-4 items-center">
+          <img
+            src="./logo.png"
+            alt="Wspieram bo wiem logo"
+            className="w-10 h-10 lg:h-16 lg:w-16"
+          />{" "}
+          <span className="text-lg lg:text-3xl italic text-primary">
+            Wspieram, bo wiem...
+          </span>
+        </div>
 
         <div>
-          <BiMenu size={30} onClick={handleNav} className="md:hidden z-[140]" />
+          <BiMenu
+            size={30}
+            onClick={handleNav}
+            className="md:hidden z-[140] text-primary"
+          />
         </div>
-        <div className="hidden md:inline-flex gap-10 items-center hover:text-primary">
+        <div className="hidden md:inline-flex md:gap-4 lg:gap-8 items-center hover:text-primary">
           <a href="/about">Про мене</a>
           <a href="/services">Послуги</a>
           <a href="#contacts">Контакти</a>
+          <a href="#" className="link-button">
+            Запис на консультацію
+          </a>
         </div>
       </div>
     </div>
